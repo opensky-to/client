705ee09a7104d3a9d01113e219c6367a097b8584
sushi.at <sushi@opensky.to>
2021-07-01 17:59:28 +0100

    Finished basic settings and started data import
    Cleaned up design colors a bit
    Created OpenSky page user control

---
+++ OpenSky.Client/Pages/Models/DataImportViewModel.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataImportViewModel.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.Client.Pages.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using OpenSky.Client.MVVM;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Data import view model.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 01/07/2021.
+    /// </remarks>
+    /// <seealso cref="T:OpenSky.Client.MVVM.ViewModel"/>
+    /// -------------------------------------------------------------------------------------------------
+    public class DataImportViewModel : ViewModel
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataImportViewModel"/> class.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 01/07/2021.
+        /// </remarks>
+        /// -------------------------------------------------------------------------------------------------
+        public DataImportViewModel()
+        {
+            // Create commands
+            this.BrowseLittleNavmapMSFSCommand = new Command(this.BrowseLittleNavmapMSFS);
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Browse for Little Navmap MSFS SQLite database file.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 01/07/2021.
+        /// </remarks>
+        /// -------------------------------------------------------------------------------------------------
+        private void BrowseLittleNavmapMSFS()
+        {
+            
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the browse little navmap msfs command.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public Command BrowseLittleNavmapMSFSCommand { get; }
+    }
+}
