e9a735f1c3b9b05c31ec8aec07dcc73d3461ed9d
sushi.at <sushi@opensky.to>
2021-07-02 13:57:53 +0100

    Finished data import page
    Created style for opensky page

--- OpenSky.Client/Pages/DataImport.xaml.cs
+++ OpenSky.Client/Pages/DataImport.xaml.cs
@@ -6,8 +6,11 @@
 
 namespace OpenSky.Client.Pages
 {
+    using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
+
+    using DataGridExtensions;
 
     /// -------------------------------------------------------------------------------------------------
     /// <content>
@@ -29,6 +32,39 @@ namespace OpenSky.Client.Pages
             this.InitializeComponent();
         }
 
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Clears all filters on click.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/07/2021.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Routed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void ClearAllFiltersOnClick(object sender, RoutedEventArgs e)
+        {
+            this.DataImportsGrid.GetFilter().Clear();
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Textbox text changed (auto scrolls to the end)
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/07/2021.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Text changed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
         private void TextBoxBaseOnTextChanged(object sender, TextChangedEventArgs e)
         {
             if (sender is TextBoxBase textBox)
