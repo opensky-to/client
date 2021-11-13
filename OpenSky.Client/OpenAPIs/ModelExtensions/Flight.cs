02eb36502ab8be7d34b39817467911e3c7d720de
sushi.at <sushi@opensky.to>
2021-11-13 21:46:02 +0000

    Added flight position/saved image columns

--- OpenSky.Client/OpenAPIs/ModelExtensions/Flight.cs
+++ OpenSky.Client/OpenAPIs/ModelExtensions/Flight.cs
@@ -19,5 +19,37 @@ namespace OpenSkyApi
         /// </summary>
         /// -------------------------------------------------------------------------------------------------
         public ImageSource FlightPausedImage => new BitmapImage(new Uri($"pack://application:,,,/OpenSky.Client;component/Resources/{(this.Paused.HasValue ? "pause16.png" : "departure16.png")}"));
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the flight position report image.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public ImageSource FlightPositionImage => new BitmapImage(new Uri($"pack://application:,,,/OpenSky.Client;component/Resources/{((this.Latitude.HasValue && this.Longitude.HasValue) ? "pin16.png" : "x16.png")}"));
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the flight saved image.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public ImageSource FlightSavedImage => new BitmapImage(new Uri($"pack://application:,,,/OpenSky.Client;component/Resources/{(this.HasAutoSaveLog ? "save16.png" : "x16.png")}"));
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets status information describing the flight (ground?, speed, altitude, heading).
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public string FlightStatus
+        {
+            get
+            {
+                if (this.OnGround)
+                {
+                    return $"On ground, {this.GroundSpeed ?? 0} kts";
+                }
+
+                return $"Airborne {this.Altitude ?? 0} ft, {this.AirspeedTrue ?? 0} kts, heading {this.Heading ?? 0}";
+            }
+        }
     }
 }
