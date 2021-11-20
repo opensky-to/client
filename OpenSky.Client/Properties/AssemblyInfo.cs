5ae34441bbe3cfc7d4b0cdd633bc00c43bf32e90
sushi.at <sushi@opensky.to>
2021-11-20 17:46:35 +0000

    Bumped version to 0.1.1 for next alpha client release

--- OpenSky.Client/Properties/AssemblyInfo.cs
+++ OpenSky.Client/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Windows.Markup;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
 // This allows us to detect debug mode in XAML
 #if DEBUG
