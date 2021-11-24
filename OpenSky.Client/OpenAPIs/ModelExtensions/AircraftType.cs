4db3f561e98d2baeef6ba9a0593d3871fca47d03
sushi.at <sushi@opensky.to>
2021-11-24 00:43:02 +0000

    Expanded aircraft market with new sales agreement
    Set only vanilla to false on default
    Added new icons for vanilla/addon aircraft

--- OpenSky.Client/OpenAPIs/ModelExtensions/AircraftType.cs
+++ OpenSky.Client/OpenAPIs/ModelExtensions/AircraftType.cs
@@ -5,8 +5,13 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 // ReSharper disable once CheckNamespace
+
 namespace OpenSkyApi
 {
+    using System;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+
     /// -------------------------------------------------------------------------------------------------
     /// <summary>
     /// Aircraft type extensions.
@@ -17,6 +22,22 @@ namespace OpenSkyApi
     /// -------------------------------------------------------------------------------------------------
     public partial class AircraftType
     {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the name with variant star.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public string NameWithVariant => $"{this.Name}{(this.HasVariants ? "*" : string.Empty)}";
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the vanilla/addon image.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public ImageSource VanillaAddonImage => new BitmapImage(
+            new Uri(
+                $"pack://application:,,,/OpenSky.Client;component/Resources/{(this.IsVanilla ? "vanilla16.png" : "addon16.png")}"));
+
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
         /// Returns a string that represents the current object.
