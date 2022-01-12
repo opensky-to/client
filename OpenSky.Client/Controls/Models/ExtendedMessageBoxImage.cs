6823263b753dd1c45f6235b613546b2e4dfb4085
sushi.at <sushi@opensky.to>
2022-01-12 15:10:16 +0000

    Added first version of open sky notifications

---
+++ OpenSky.Client/Controls/Models/ExtendedMessageBoxImage.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExtendedMessageBoxImage.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.Client.Controls.Models
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Extended message box image enumeration.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 12/01/2022.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public enum ExtendedMessageBoxImage
+    {
+
+        /// <summary>
+        /// White large star.
+        /// </summary>
+        Asterisk,
+
+        /// <summary>
+        /// Green circle white white check-mark.
+        /// </summary>
+        Check,
+
+        /// <summary>
+        /// Red circle with white "X".
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Yellow triangle with black "!".
+        /// </summary>
+        Exclamation,
+
+        /// <summary>
+        /// Red stop sign with white hand.
+        /// </summary>
+        Hand,
+
+        /// <summary>
+        /// Blue circle with white "i".
+        /// </summary>
+        Information,
+
+        /// <summary>
+        /// None, use default OpenSky logo.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Blue speech bubble with white "?".
+        /// </summary>
+        Question,
+
+        /// <summary>
+        /// Red octagon with white "STOP".
+        /// </summary>
+        Stop,
+
+        /// <summary>
+        /// Dark-yellow circle with white "!".
+        /// </summary>
+        Warning
+    }
+}
--- OpenSky.Client/Controls/OpenSkyNotification.xaml.cs
+++ OpenSky.Client/Controls/OpenSkyNotification.xaml.cs
@@ -6,9 +6,12 @@
 
 namespace OpenSky.Client.Controls
 {
+    using System;
     using System.Threading;
     using System.Windows;
+    using System.Windows.Media;
 
+    using OpenSky.Client.Controls.Models;
     using OpenSky.Client.Tools;
 
     /// -------------------------------------------------------------------------------------------------
@@ -20,52 +23,284 @@ namespace OpenSky.Client.Controls
     {
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
+        /// The automatic close result.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private readonly MessageBoxResult autoCloseResult;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The automatic close timeout.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private int autoCloseTimeout;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
         /// Initializes a new instance of the <see cref="OpenSkyNotification"/> class.
         /// </summary>
         /// <remarks>
         /// sushi.at, 10/01/2022.
         /// </remarks>
+        /// <param name="title">
+        /// The title.
+        /// </param>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <param name="buttons">
+        /// (Optional) The buttons.
+        /// </param>
+        /// <param name="image">
+        /// (Optional) The image.
+        /// </param>
+        /// <param name="autoCloseTimeout">
+        /// (Optional)
+        /// The automatic close timeout.
+        /// </param>
+        /// <param name="autoCloseResult">
+        /// (Optional) The automatic close result.
+        /// </param>
         /// -------------------------------------------------------------------------------------------------
-        public OpenSkyNotification(string title, string message, MessageBoxButton? buttons = null, MessageBoxImage image = MessageBoxImage.None, int autoCloseTimeout = 0)
+        public OpenSkyNotification(string title, string message, MessageBoxButton? buttons = null, ExtendedMessageBoxImage image = ExtendedMessageBoxImage.None, int autoCloseTimeout = 0, MessageBoxResult autoCloseResult = MessageBoxResult.None)
         {
             this.InitializeComponent();
 
             this.Title.Text = title;
             this.Message.Text = message;
 
-            if (autoCloseTimeout > 0)
+            if (buttons.HasValue)
             {
-                this.TimeoutText.Text = $"{autoCloseTimeout}";
-                this.TimeoutProgress.Maximum = autoCloseTimeout;
+                this.ButtonPanel.Visibility = Visibility.Visible;
+                switch (buttons.Value)
+                {
+                    case MessageBoxButton.OK:
+                        this.OkButton.Visibility = Visibility.Visible;
+                        break;
+                    case MessageBoxButton.OKCancel:
+                        this.OkButton.Visibility = Visibility.Visible;
+                        this.CancelButton.Visibility = Visibility.Visible;
+                        break;
+                    case MessageBoxButton.YesNo:
+                        this.YesButton.Visibility = Visibility.Visible;
+                        this.NoButton.Visibility = Visibility.Visible;
+                        break;
+                    case MessageBoxButton.YesNoCancel:
+                        this.YesButton.Visibility = Visibility.Visible;
+                        this.NoButton.Visibility = Visibility.Visible;
+                        this.CancelButton.Visibility = Visibility.Visible;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(buttons), buttons.Value, null);
+                }
+            }
+
+            switch (image)
+            {
+                case ExtendedMessageBoxImage.Asterisk:
+                    this.Image.Source = this.FindResource("Asterisks") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Check:
+                    this.Image.Source = this.FindResource("Check") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Error:
+                    this.Image.Source = this.FindResource("Error") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Exclamation:
+                    this.Image.Source = this.FindResource("Exclamation") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Hand:
+                    this.Image.Source = this.FindResource("Hand") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Information:
+                    this.Image.Source = this.FindResource("Info") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.None:
+                    break;
+                case ExtendedMessageBoxImage.Question:
+                    this.Image.Source = this.FindResource("Question") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Stop:
+                    this.Image.Source = this.FindResource("Stop") as DrawingImage;
+                    break;
+                case ExtendedMessageBoxImage.Warning:
+                    this.Image.Source = this.FindResource("Warning") as DrawingImage;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(image), image, null);
+            }
+
+            this.autoCloseTimeout = autoCloseTimeout;
+            this.autoCloseResult = autoCloseResult;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the notification background brush.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public Brush NotificationBackground
+        {
+            get => this.BackgroundBorder.Background;
+            set => this.BackgroundBorder.Background = value;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Occurs when the notification is closed.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public event EventHandler Closed;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the notification result.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public MessageBoxResult Result { get; private set; } = MessageBoxResult.None;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Cancel button click.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/01/2022.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Routed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void CancelButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.Result = MessageBoxResult.Cancel;
+            this.Visibility = Visibility.Collapsed;
+            this.Closed?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// No button click.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/01/2022.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Routed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void NoButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.Result = MessageBoxResult.No;
+            this.Visibility = Visibility.Collapsed;
+            this.Closed?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Ok button click.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/01/2022.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Routed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void OkButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.Result = MessageBoxResult.OK;
+            this.Visibility = Visibility.Collapsed;
+            this.Closed?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// OpenSky notification loaded.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 11/01/2022.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Routed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void OpenSkyNotificationOnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+
+            if (this.autoCloseTimeout > 0)
+            {
+                this.TimeoutText.Text = $"{this.autoCloseTimeout}";
+                this.TimeoutProgress.Maximum = this.autoCloseTimeout;
                 this.TimeoutText.Visibility = Visibility.Visible;
                 this.TimeoutProgress.Visibility = Visibility.Visible;
                 new Thread(
-                    () =>
-                    {
-                        while (autoCloseTimeout > 0)
+                        () =>
                         {
-                            Thread.Sleep(1000);
-                            if (SleepScheduler.IsShutdownInProgress)
+                            while (this.autoCloseTimeout > 0 && this.Visibility == Visibility.Visible)
                             {
-                                return;
+                                Thread.Sleep(1000);
+                                if (SleepScheduler.IsShutdownInProgress)
+                                {
+                                    return;
+                                }
+
+                                this.autoCloseTimeout--;
+                                UpdateGUIDelegate updateTimeout = () =>
+                                {
+                                    this.TimeoutText.Text = $"{this.autoCloseTimeout}";
+                                    this.TimeoutProgress.Progress = this.autoCloseTimeout;
+                                };
+                                this.Dispatcher.BeginInvoke(updateTimeout);
                             }
 
-                            autoCloseTimeout--;
-                            UpdateGUIDelegate updateTimeout = () =>
+                            UpdateGUIDelegate hideNotification = () =>
                             {
-                                // ReSharper disable AccessToModifiedClosure
-                                this.TimeoutText.Text = $"{autoCloseTimeout}";
-                                this.TimeoutProgress.Progress = autoCloseTimeout;
-                                // ReSharper restore AccessToModifiedClosure
-                            };
-                            this.Dispatcher.BeginInvoke(updateTimeout);
-                        }
+                                if (this.autoCloseTimeout == 0)
+                                {
+                                    this.Result = this.autoCloseResult;
+                                }
 
-                        UpdateGUIDelegate hideNotification = () => this.Visibility = Visibility.Collapsed;
-                        this.Dispatcher.BeginInvoke(hideNotification);
-                    })
-                { Name = "OpenSkyNotification.AutoCloseTimeout" }.Start();
+                                this.Visibility = Visibility.Collapsed;
+                                this.Closed?.Invoke(this, EventArgs.Empty);
+                            };
+                            this.Dispatcher.BeginInvoke(hideNotification);
+                        })
+                    { Name = "OpenSkyNotification.AutoCloseTimeout" }.Start();
             }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Yes button click.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/01/2022.
+        /// </remarks>
+        /// <param name="sender">
+        /// Source of the event.
+        /// </param>
+        /// <param name="e">
+        /// Routed event information.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void YesButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.Result = MessageBoxResult.Yes;
+            this.Visibility = Visibility.Collapsed;
+            this.Closed?.Invoke(this, EventArgs.Empty);
         }
     }
 }
